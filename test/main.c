fec74afc78a227ed31d0785f8f312260bd607518
thousandmiles <longdaluoshen@gmail.com>
2023-11-27 16:54:50 +0800

    test microhttpd

--- test/main.c
+++ test/main.c
@@ -3,19 +3,36 @@
 #include "memory.h"
 #include "process.h"
 #include "stdio.h"
+#include "httpserver.h"
+#include <microhttpd.h>
 
 int main(void)
 {
-    test_process();
+    // test_process();
 
-    printf("-------------------------------------------\n");
-    test_cpu();
+    // printf("-------------------------------------------\n");
+    // test_cpu();
 
-    printf("-------------------------------------------\n");
-    test_memory();
+    // printf("-------------------------------------------\n");
+    // test_memory();
 
-    printf("-------------------------------------------\n");
-    test_disk();
+    // printf("-------------------------------------------\n");
+    // test_disk();
+
+    struct MHD_Daemon *daemon;
+
+    daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
+                              &answer_to_connection, NULL, MHD_OPTION_END);
+    if (NULL == daemon)
+    {
+        perror("daemon failed!");
+        return 1;
+    }
+
+    getchar();
+
+    MHD_stop_daemon(daemon);
+    return 0;
 
     return 0;
 }
