ec6a9173e3309dd7e6184aace1e0aa1e069e3b72
thousandmiles <longdaluoshen@gmail.com>
2023-12-02 10:26:17 +0800

    add command parameters

--- run/main.c
+++ run/main.c
@@ -17,43 +17,97 @@ int main(int argc, char *argv[])
 {
 
     int opt;
+    char *username = NULL;
+    char *password = NULL;
+    char *ip_address = NULL;
+    int run_in_background = 0;
+    int debug_mode = 0;
 
-    while ((opt = getopt(argc, argv, "abc:")) != -1)
+    while ((opt = getopt(argc, argv, "u:p:d:b")) != -1)
     {
         switch (opt)
         {
-        case 'a':
-            printf("do a...\n");
+        case 'u':
+            username = optarg;
+            break;
+        case 'p':
+            password = optarg;
+            break;
+        case 'd':
+            debug_mode = 1;
+            ip_address = optarg;
             break;
         case 'b':
-            printf("do b...\n");
-            break;
-        case 'c':
-            printf("do c...\n");
+            run_in_background = 1;
             break;
         default:
-            printf("do default...\n");
             break;
         }
     }
 
-    char *action;
-    for (int i = optind; i < argc; i++)
+    if (optind >= argc)
     {
-        action = argv[i];
-        if (strcmp(action, "run") == 0)
+        fprintf(stderr, "please input parameters, see help by command: 'CrossNex help'\n");
+        return EXIT_FAILURE;
+    }
+
+    char *action = argv[optind];
+
+    if (strcmp(action, "run") == 0)
+    {
+        if (run_in_background && !debug_mode)
         {
-            printf("do run...\n");
+            printf("background running... \n");
         }
-        else if (strcmp(action, "stop") == 0)
+        else if (run_in_background && debug_mode)
         {
-            printf("do stop...\n");
+            printf("background debug running: %s\n", ip_address);
+        }
+        else if (!run_in_background && debug_mode)
+        {
+            printf("debug running...\n");
         }
         else
         {
-            fprintf(stderr, "unknown cmd: %s\n", action);
+            printf("default running...\n");
+        }
+    }
+    else if (strcmp(action, "stop") == 0)
+    {
+        printf("stop running...\n");
+    }
+    else if (strcmp(action, "add") == 0)
+    {
+        if (username != NULL && password != NULL)
+        {
+            printf("add users: %s, password: %s\n", username, password);
+        }
+        else
+        {
+            fprintf(stderr, "username or password is empty!\n");
             return EXIT_FAILURE;
         }
+    }
+    else if (strcmp(action, "delete") == 0)
+    {
+        if (username != NULL)
+        {
+            printf("delete user: %s\n", username);
+        }
+        else
+        {
+            fprintf(stderr, "user name is empty!\n");
+            return EXIT_FAILURE;
+        }
+    }
+    else if (strcmp(action, "help") == 0)
+    {
+        printf("help page ...\n");
+    }
+    else
+    {
+        fprintf(stderr, "undefined paramaters: %s\n", action);
+        return EXIT_FAILURE;
     }
     return 0;
 
